d7becc41be46bcca3be527eca67c2f3c163f96c6
Muhammad Ali Hamza <71075942+aliansari2566@users.noreply.github.com>
2023-10-12 19:57:56 +0500

    index.js in backend updated

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const cors = require("cors")
-const mongoose = require("mongoose")
+
 const userRoutes = require("./routes/userRoutes")
 const connectDB = require("./db");
 
