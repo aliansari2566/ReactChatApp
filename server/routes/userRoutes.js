2bef14d020e129b51f64e480bb9a42c434e5d622
Muhammad Ali Hamza <71075942+aliansari2566@users.noreply.github.com>
2023-11-13 17:09:25 +0500

    new changes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,5 +5,6 @@ const router = require("express").Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/setAvatar/:id", setAavatar);
 
 module.exports = router;
